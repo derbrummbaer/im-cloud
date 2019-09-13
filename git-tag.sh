ed94574f0b5fbf86555949f3eb692a942568649a
Sven Berger <sven@berger-lembeck.de>
2019-09-13 06:32:12 +0200

    Nextcloud 15.0.11 / Collabora 4.0.5.2 / MariaDB 10.4

--- git-tag.sh
+++ git-tag.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 nextcloud="15.0.11"
-ext=".0"
+ext=".2"
 office="4.0.5.2"
+mariadb="10.4"
 tagversion=$nextcloud$ext
 
-git tag -a $tagversion -m "Nextcloud "$nextcloud" / Collabora "$collabora
+git add .
+git commit . -m "Nextcloud "$nextcloud" / Collabora "$office" / MariaDB "$mariadb
+git tag -a $tagversion -m "Nextcloud "$nextcloud" / Collabora "$office" / MariaDB "$mariadb
 git push -u origin master
 git push -u origin --tag
